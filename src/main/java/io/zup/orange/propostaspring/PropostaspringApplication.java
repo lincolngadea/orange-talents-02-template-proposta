edc250e1eb535e4d9c036476ca4f7a12e97e76e3
Lincoln Gadea <lincoln@glinc.com.br>
2021-03-15 09:39:00 -0300

    chore: first commit

---
+++ src/main/java/io/zup/orange/propostaspring/PropostaspringApplication.java
@@ -0,0 +1,13 @@
+package io.zup.orange.propostaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/zup/orange/propostaspring/PropostaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zup.orange.propostaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
